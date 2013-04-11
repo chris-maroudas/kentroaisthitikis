6ceafe8f55901d9a94fa7bb7fe2e49cb1e1df50b
Grigoria <ggoria@gmail.com>
2013-04-11 23:07:11 +0300

    Add About us page (to kentro)

--- about_us.php
+++ about_us.php
@@ -1,8 +1,40 @@
 <?php include 'header.php'; ?>
     
     <div id="kentro" class="wrapper">
-
-    
+        
+        <img src="images/about_us.jpg" />
+        <article>
+            <p>Το επιστημονικό κέντρο Αισθητικής Ιατρικής & Κοσμητολογίας προσώπου – σώματος μόλις λίγο έξω από το κέντρο της Γλυφάδας στην περιοχή της Αιξώνης,
+            ανταποκρίνεται πλήρως στην εφαρμογή εξειδικευμένων μεθόδων για την περιποίηση του σώματος και του προσώπου έχοντας ως προϋπόθεση την χρήση επιστημονικών
+            μεθόδων από το χώρο της αισθητικής καθώς και πρωτοποριακών καλλυντικών προϊόντων, αναστέλλοντας αρχικά και επιλύοντας οριστικά στη συνέχεια προβλήματα αισθητικής,
+            τοπικού πάχους, κυτταρίτιδας, ανεπιθύμητης τριχοφυΐας κ.λ.π. </p>
+            <p>Σκοπός μας είναι η παροχή υψηλής στάθμης θεραπειών προληπτικής και θεραπευτικής αγωγής από το χώρο της αισθητικής και της κοσμητολογίας, με στόχο την βελτίωση
+            των προβλημάτων και την αποκατάστασή τους με απόλυτη υπευθυνότητα και αποτελεσματικότητα. </p>
+            <p>Στο κέντρο μας, σε συνεργασία με επιστήμονες διαιτολόγους, σας παρέχουμε συμβουλές και λύσεις για υγιεινή διατροφή και απώλεια βάρους.</p>
+            <p>Σ' ένα όμορφο, ζεστό και φιλικό περιβάλλον χαλαρώστε και απολαύστε υπηρεσίες  ομορφιάς και μασάζ  σε τιμές φιλικές!
+            Τα εξειδικευμένα φυτικά προιόντα που χρησιμοποιούνται, σέβονται, αγαπούν, αναζοωγονούν το δέρμα και εξισορροπούν την ψυχή και το σώμα!
+            Χαρίστε στο εαυτό σας αλλά και στα αγαπημένα σας πρόσωπα, στιγμές ομορφιάς και χαλάρωσης με καταπληκτικές θεραπείες προσώπου - σώματος
+            σε τιμές προσαρμοσμένες στις οικονομικές δυσκολίες της εποχής μας!</p>
+        </article>
+        
+        <article class="bio">
+            <img src="images/eirini.jpg" />
+            <p>Gummi bears marshmallow chocolate cake. Wypas powder gingerbread tiramisu macaroon chocolate chupa chups. Wypas chocolate cake lollipop cheesecake cheesecake
+            oat cake chocolate bar. Cheesecake dessert macaroon croissant. Icing caramels liquorice sweet jujubes bonbon gingerbread. Brownie applicake topping wypas liquorice
+            ice cream toffee gingerbread. Macaroon wypas jelly-o chocolate. Faworki liquorice pastry fruitcake tootsie roll tart chupa chups. Bonbon pudding applicake cheesecake oat cake.</p>
+        </article>
+        
+        <section>
+            <h2>Δείτε το χώρο μας</h2>
+            <ul class="cf">
+                <li><img src="images/xoros.jpg" /></li>
+                <li><img src="images/xoros.jpg" /></li>
+                <li><img src="images/xoros.jpg" /></li>
+                <li><img src="images/xoros.jpg" /></li>
+                <li><img src="images/xoros.jpg" /></li>
+                <li><img src="images/xoros.jpg" /></li>
+            </ul>
+        </section>
     
     </div>
     
