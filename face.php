52c12da9ebc183d4bf8efd447d9f3ca030077875
Grigoria <ggoria@gmail.com>
2013-04-25 23:15:40 +0300

    Create all pages for future use

---
+++ face.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+
+    <div id="services" class="wrapper">
+        <h2>Περιποίηση προσώπου</h2>
+        
+        <section>
+            
+        </section>
+    </div>
+    
+<?php include 'footer.php'; ?>
